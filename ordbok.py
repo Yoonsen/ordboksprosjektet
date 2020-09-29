7d08a09aefff67b1a07f8f1cd6b7f0aae1fbfcc9
Lars G Johnsen <yoonsen@gmail.com>
2020-09-29 17:17:38 +0200

    Update ordbok.py

--- ordbok.py
+++ ordbok.py
@@ -136,7 +136,7 @@ konk_ord = st.text_input('konkordanseord', list(resultat.index)[0])
 konks = nb.concordance(konk_ord, corpus='bok', yearfrom = period_slider[0], yearto = period_slider[1], size = 20, kind='json')
 
 st.markdown('\n\n'.join([ str(j['before']) + ' _' + str(j['word']) + '_ ' + str(j['after']) \
-            + ' $\\bullet$  [' + str(j['title']) + '](' + str(j['urn']) + '), ' + \
+            + ' $\\bullet$  [' + str(j['title']) + '](' + str(j['urn']) + '?searchText=' + konk_ord + '), ' + \
           j['author'] + ', ' + str(j['year']) for j in konks]
                        ))
 #st.write(konks)
